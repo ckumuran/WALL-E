095119ff92ee9e3ab96432cb39e4d126636691f2
Kumaran Chandrashekar <k31311558@gmail.com>
2026-06-21 12:25:02 +0530

    Delete attacks.h

--- attacks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "types.h"
-extern U64 pawnAttacks[2][64];
-extern U64 knightAttacks[64];
-extern U64 kingAttacks[64];
-void init_leapers();
-U64 rook_attacks(int sq, U64 occ);
-U64 bishop_attacks(int sq, U64 occ);
-U64 queen_attacks(int sq, U64 occ);
