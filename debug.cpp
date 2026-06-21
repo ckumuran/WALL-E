5928470e8d3dc564a4ea04d61ba63b606bdbc429
Kumaran Chandrashekar <k31311558@gmail.com>
2026-06-21 12:28:47 +0530

    Delete debug.cpp

--- debug.cpp
+++
@@ -1,6 +0,0 @@
-#include "debug.h"
-bool board_ok(const Board& board) {
-    int whiteKings = __builtin_popcountll(board.pieces[WHITE_KING]);
-    int blackKings = __builtin_popcountll(board.pieces[BLACK_KING]);
-    return whiteKings == 1 && blackKings == 1;
-}
